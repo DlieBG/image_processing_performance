a72d9932a6704df59a3994d5211ebd9893661c9d
Benedikt Schwering <mail@bschwer.ing>
2024-12-02 13:07:01 +0100

    feat(utils): add python project image utils

--- python/src/algorithms/background_subtraction.py
+++ python/src/algorithms/background_subtraction.py
@@ -8,6 +8,7 @@ It can be found in the LICENSE file or at https://opensource.org/licenses/MIT.
 
 Author Benedikt SCHWERING <bes9584@thi.de>
 """
+from src.utils.image import open_project_image, save_project_image
 from pathlib import Path
 
 def background_subtraction(delta: float, input_path: Path, output_path: Path):
@@ -21,4 +22,13 @@ def background_subtraction(delta: float, input_path: Path, output_path: Path):
             input_path (Path): Input path for image files.
             output_path (Path): Output path for image files.
     """
-    pass
+    project_image = open_project_image(
+        image_path=input_path,
+    )
+
+    # Todo: Implement Background Subtraction Algorithm
+
+    save_project_image(
+        image_path=output_path,
+        project_image=project_image,
+    )
--- python/src/utils/image.py
+++ python/src/utils/image.py
@@ -44,25 +44,17 @@ def open_project_image(image_path: Path) -> Image:
     # Convert the flat data to a list of pixels.
     flat_pixels = [
         Pixel(
-            red=flat_data[i],
-            green=flat_data[i + 1],
-            blue=flat_data[i + 2],
+            red=flat_pixel[0],
+            green=flat_pixel[1],
+            blue=flat_pixel[2],
         )
-            for i in range(
-                start=0,
-                stop=len(flat_data),
-                step=3,
-            )
+            for flat_pixel in flat_data
     ]
 
     # Convert the pixels to a two dimensional list.
     pixels = [
         flat_pixels[i:i + width]
-            for i in range(
-                start=0,
-                stop=len(flat_pixels),
-                step=width,
-            )
+            for i in range(0, len(flat_pixels), width)
     ]
 
     # Create a new project image and return it.
@@ -82,4 +74,31 @@ def save_project_image(image_path: Path, project_image: Image):
             image_path (Path): Path to the image.
             project_image (Image): Project image.
     """
-    pass
+    # Create a new PIL image with the same width and height as the project image.
+    pil_image = PILImage.new(
+        mode='RGB',
+        size=(project_image.width, project_image.height),
+    )
+
+    # Get the flat pixels of the project image two dimensional pixels.
+    flat_pixels = [
+        pixel
+            for row in project_image.pixels
+                for pixel in row
+    ]
+
+    # Get the flat data of the project image flat pixels.
+    flat_data = [
+        (pixel.red, pixel.green, pixel.blue)
+            for pixel in flat_pixels
+    ]
+
+    # Put the flat data into the PIL image.
+    pil_image.putdata(
+        data=flat_data,
+    )
+
+    # Save the PIL image to the file.
+    pil_image.save(
+        fp=image_path,
+    )
